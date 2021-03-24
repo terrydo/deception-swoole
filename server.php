763894eb85d99ea7054306df61d8a340c24c91ed
Do Bao Trung <trungdb@smartosc.com>
2021-03-24 18:27:01 +0700

    Update

--- server.php
+++ server.php
@@ -1,13 +1,9 @@
-<?php
-$http = new Swoole\HTTP\Server("0.0.0.0", 9501);
+<?php 
 
-$http->on('start', function ($server) {
-    echo "Swoole http server is started at http://127.0.0.1:9501\n";
-});
+	require_once 'deception-server.php';
 
-$http->on('request', function ($request, $response) {
-    $response->header("Content-Type", "text/plain");
-    $response->end("Hello World\n");
-});
+	$server = new DeceptionServer("0.0.0.0", 9502);
 
-$http->start();
+	$server->start();
+	
+?>
